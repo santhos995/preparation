95b9721ea2f082337dc971fbfe00652db98935c0
santhos995 <santhos995@gmail.com>
2019-06-16 20:16:33 +0530

    Yield keywork practice

---
+++ YieldPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            foreach(int val in yieldMethod())
+            {
+                Console.WriteLine(val);
+            }
+        }
+
+        private static IEnumerable<int> yieldMethod()
+        {
+            for(int i=0;i<10;i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
